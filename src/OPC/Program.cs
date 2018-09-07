492f3dc562422c017d6de923071a38e21879c688
Miguel Pérez García <miguelpg_95@hotmail.com>
2018-09-06 21:02:11 -0500

    Initial project

---
+++ src/OPC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
