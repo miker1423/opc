07b69cb75e55b9ac0231163420ea4a48959f71f3
Miguel Pérez García <miguelpg_95@hotmail.com>
2018-09-11 00:22:56 -0500

    starting syntax analyzer

--- src/OPC.Core/SyntaxModule.fs
+++ src/OPC.Core/SyntaxModule.fs
@@ -1,4 +1,20 @@
-﻿
-module SyntaxModule 
+﻿namespace OPC.Core
 
+open OPC.Core.Types
 
+module SyntaxModule = 
+    
+    let transform(list:List<Tokens>) =
+        list |> Seq.toList 
+
+    let syntaxAnalyze(tokenList:List<Tokens>) =
+        let tokens = transform tokenList
+        match tokens with 
+        | head :: tail -> 
+            match head with 
+            | Tokens.Keyword (_, dt) -> 
+                match dt with 
+                | DataTypes.None | DataTypes.LogicConstant -> false
+                | _ -> true
+            | _ -> false
+        | _ -> false
